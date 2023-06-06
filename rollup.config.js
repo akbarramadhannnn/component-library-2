3961414b6d27a6455db5b321314668dd898a5852
akbarramadhannnn <akbarramadhan452@gmail.com>
2023-06-07 00:43:11 +0700

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+// import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/*",
+        presets: ["@babel/preset-react"],
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      //   typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+    ],
+  },
+  {
+    input: "dist/esm/index.js",
+    output: [{ file: "dist/index.d.js", format: "esm" }],
+    plugins: [dts()],
+    // external: [/\.css$/],
+  },
+];
---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import "./Button.css";
+
+const Button = () => {
+  return <button className="button">Testing Button</button>;
+};
+
+export default memo(Button);
